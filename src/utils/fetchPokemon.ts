6c0f4f05802cb6e62e72d693c4df3960ff410a24
Victor Granda <victorgranda1992@gmail.com>
2023-01-29 14:06:36 +0100

    Add random function and rebuild pokemonDetails

--- src/utils/fetchPokemon.ts
+++ src/utils/fetchPokemon.ts
@@ -1,25 +1,36 @@
-import type { InfoPokedex, InfoPokemons } from "../components/types.js";
+import type {
+  InfoPokedex,
+  InfoPokemons,
+  PokeDetails,
+} from "../components/types.js";
+import { renderPokemons } from "./renderPokemon.js";
 const originalPokemons = "https://pokeapi.co/api/v2/pokemon?limit=";
 const pokeApiGeneral = "https://pokeapi.co/api/v2/pokemon/";
 
-export const fetchPokemonsClassics = async (pokeLimit: number) => {
+export const allPokemonClassicsList = async (pokeLimit: number) => {
   const response = await fetch(`${originalPokemons}${pokeLimit}`);
   const pokeData = (await response.json()) as InfoPokedex;
   pokeData.results.forEach(async (pokemon) => {
     const urlPoke = pokemon.url;
     const pokemonDetails = await fetch(urlPoke);
     const pokemons = (await pokemonDetails.json()) as InfoPokemons;
+
     return pokemons;
   });
   return pokeData;
 };
 
-export const fetchDetailsPokemon = async (idPoke: number) => {
+export function getRandomPokemon(maxPokemons: number) {
+  return Math.floor(Math.random() * maxPokemons);
+}
+
+export const pokemonDetails = async (idPoke: number) => {
   try {
     const response = await fetch(`${pokeApiGeneral}${idPoke}`);
-    const data = (await response.json()) as InfoPokedex;
+    const data = (await response.json()) as PokeDetails;
+    renderPokemons(data);
     return data;
   } catch (error: unknown) {}
 };
 
-export default fetchDetailsPokemon;
+export default pokemonDetails;
